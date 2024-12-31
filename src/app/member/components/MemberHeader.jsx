a88bf4efbdc53aa8a95e053c360c9ba9ad4238a9
Hunter Wang <s092284s@gmail.com>
2024-12-31 17:47:37 +0800

    feat: 會員頁Header增加是否登入條件判斷

--- src/app/member/components/MemberHeader.jsx
+++ src/app/member/components/MemberHeader.jsx
@@ -1,9 +1,16 @@
 'use client';
 
+import { Button } from '@mui/material';
+import { useRouter } from 'next/navigation';
+
 import BurgerMenu from '@/components/common/BurgerMenu/BurgerMenu';
 import Logo from '@/components/common/Logo/Logo';
 
+import { useAuthTypeStore } from '@/store/useAuthStore';
+
 const MemberHeader = () => {
+  const { hasToken } = useAuthTypeStore();
+  const router = useRouter();
   return (
     <header className="flex w-full fixed top-0 z-10 h-20 bg-[#ffffff]">
       <div className="p-4">
@@ -12,8 +19,25 @@ const MemberHeader = () => {
       <div className="flex flex-1 justify-between items-center">
         <div className="text-xl font-bold">會員中心</div>
         <div className=" flex gap-2 items-center mr-20">
-          <div className=" p-2">刊登廣告</div>
-          <div className=" p-2">我有物件</div>
+          <div className=" p-2">
+            <Button
+              onClick={() => {
+                if (!hasToken) {
+                  setAuthType('unauthorized');
+                  setModalOpen(true);
+                } else {
+                  router.push('/publish');
+                }
+              }}
+              sx={{
+                color: '#333333',
+                fontWeight: '700',
+              }}
+              className=" rounded-lg hover:bg-[#e9e9e9]  font-bold"
+            >
+              在TOPRE上刊登物件
+            </Button>
+          </div>
           <div className=" p-2">|</div>
           <div>
             <BurgerMenu />
